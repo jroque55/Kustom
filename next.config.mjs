1f9ea0dc794894ef35a0408b7912af02fba5472a
Juan Diego <jroque@frba.utn.edu.ar>
2026-04-02 16:04:45 -0300

    eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // Ignora avisos de ESLint al subir a Vercel
-  },
-  typescript: {
-    ignoreBuildErrors: true, // Ignora errores de tipos al subir a Vercel
-  },
+
 };
 
 export default nextConfig;
